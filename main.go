67fc9a3887559f644a28dbf93b58b508c78afa4d
André Luiz Hashiguchi <alshashiguchi@gmail.com>
2022-02-24 02:52:43 +0000

    Primeira rota com Gin

--- main.go
+++ main.go
@@ -2,7 +2,17 @@ package main
 
 import "github.com/gin-gonic/gin"
 
+func ExibeTodosAlunos(c *gin.Context) {
+	c.JSON(200, gin.H{
+		"id":   "1",
+		"nome": "Andre Luiz",
+	})
+}
+
 func main() {
 	r := gin.Default()
+
+	r.GET("/alunos", ExibeTodosAlunos)
+
 	r.Run() // roda na porta 8080 para definir outra r.Run(":5000")
 }
