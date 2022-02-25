d2c9e2d18406846430b999c4c9d33d911b418288
André Luiz Hashiguchi <alshashiguchi@gmail.com>
2022-02-25 01:55:46 +0000

    Modularizando o código

--- main.go
+++ main.go
@@ -1,18 +1,7 @@
 package main
 
-import "github.com/gin-gonic/gin"
-
-func ExibeTodosAlunos(c *gin.Context) {
-	c.JSON(200, gin.H{
-		"id":   "1",
-		"nome": "Andre Luiz",
-	})
-}
+import "github.com/alshashiguchi/api-go-gin/routes"
 
 func main() {
-	r := gin.Default()
-
-	r.GET("/alunos", ExibeTodosAlunos)
-
-	r.Run() // roda na porta 8080 para definir outra r.Run(":5000")
+	routes.HandleRequest()
 }
---
+++ routes/routes.go
@@ -0,0 +1,14 @@
+package routes
+
+import (
+	"github.com/alshashiguchi/api-go-gin/controllers"
+	"github.com/gin-gonic/gin"
+)
+
+func HandleRequest() {
+	r := gin.Default()
+
+	r.GET("/alunos", controllers.ExibeTodosAlunos)
+
+	r.Run() // roda na porta 8080 para definir outra r.Run(":5000")
+}
