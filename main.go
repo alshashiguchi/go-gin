941b9e054a6600a4c58d05ad1e40a04c8cf4c228
André Luiz Hashiguchi <alshashiguchi@gmail.com>
2022-03-03 02:34:31 +0000

    Exibindo alunos do banco

--- main.go
+++ main.go
@@ -2,17 +2,11 @@ package main
 
 import (
 	"github.com/alshashiguchi/api-go-gin/database"
-	"github.com/alshashiguchi/api-go-gin/models"
 	"github.com/alshashiguchi/api-go-gin/routes"
 )
 
 func main() {
 
 	database.ConectaComBancoDeDados()
-
-	models.Alunos = []models.Aluno{
-		{Nome: "André Luiz", CPF: "999999999-99", RG: "00.000.000-0"},
-		{Nome: "Arthur Yoshiharu", CPF: "000000000-00", RG: "11.000.000-0"},
-	}
 	routes.HandleRequest()
 }
