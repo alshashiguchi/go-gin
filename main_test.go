09bbccd8ad6738665105ac0dc9d7eaa03ea16644
André Luiz Hashiguchi <alshashiguchi@gmail.com>
2022-08-24 01:23:09 +0000

    Testando a listagem de um recurso

--- main_test.go
+++ main_test.go
@@ -7,6 +7,7 @@ import (
 	"testing"
 
 	"github.com/alshashiguchi/api-go-gin/controllers"
+	"github.com/alshashiguchi/api-go-gin/database"
 	"github.com/gin-gonic/gin"
 	"github.com/stretchr/testify/assert"
 )
@@ -29,4 +30,21 @@ func TestVerificaStatusCodeDaSaudacaoComParametro(t *testing.T) {
 
 	assert.Equal(t, http.StatusOK, res.Code, "Deveriam ser iguais")
 	assert.Equal(t, mockDaResposta, string(respostaBody))
+}
+
+func TestListandoTodosOsAlunosHandler(t *testing.T) {
+	// Arrange
+	database.ConectaComBancoDeDados()
+
+	r := SetupDasRotasDeTeste()
+	r.GET("/alunos", controllers.ExibeTodosAlunos)
+
+	req, _ := http.NewRequest("GET", "/alunos", nil)
+	res := httptest.NewRecorder()
+
+	// act
+	r.ServeHTTP(res, req)
+
+	//Assert
+	assert.Equal(t, http.StatusOK, res.Code, "Deveriam ser iguais")
 }
